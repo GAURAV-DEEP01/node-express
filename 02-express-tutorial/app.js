969e24dacf1ea8ebaa789bccb36f78b596f099dc
Gaurav <gauravdeep8660@gmail.com>
2023-07-26 23:02:18 +0530

    learn 2

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,16 @@
 const http = require('http')
+const path = require('path')
 const express = require('express')
 const app = express()
 
+app.use(express.static('./public'))
+
 app.get('/about',(req,res)=>{
-    res.send('homepage')
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>oops</h1>')
 })
 app.listen(8000,()=>{
     console.log('listening 5000')
