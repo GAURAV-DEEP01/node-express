7e5a806abc9cbc29a0522c1577282fbfe17d6a93
Gaurav <gauravdeep8660@gmail.com>
2023-07-26 22:16:49 +0530

    learn 1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+const http = require('http')
+const express = require('express')
+const app = express()
+
+app.get('/about',(req,res)=>{
+    res.send('homepage')
+})
+app.listen(8000,()=>{
+    console.log('listening 5000')
+})
