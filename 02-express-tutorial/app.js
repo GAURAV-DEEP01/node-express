0cd3e510ad1679bb6eb270a4efbaba835159373d
Gaurav <gauravdeep8660@gmail.com>
2023-07-28 22:48:00 +0530

    3

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,16 +2,23 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const app = express()
+const product = require('./json')
+const port = 8000
 
 app.use(express.static('./public'))
 
 app.get('/about',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
-
+app.get('/getFeedback/feedback/:feedbackId/rating/:ratingper',(request,response)=>{
+    console.log(request.params)
+    response.json(product)
+    const {feedbackId,ratingper} = request.params
+    response.status(200).send(`${feedbackId}${ratingper}`)
+})
 app.all('*',(req,res)=>{
     res.status(404).send('<h1>oops</h1>')
 })
-app.listen(8000,()=>{
-    console.log('listening 5000')
+app.listen(port,()=>{
+    console.log('listening'+ port)
 })
---
+++ 02-express-tutorial/json.js
@@ -0,0 +1,21 @@
+const mydata = [{
+    "person1": [
+      {
+        "first": "Nicole",
+        "last": "Adelstein"
+      }
+    ],
+    "person2": [
+      {
+        "first": "Pleuni",
+        "last": "Pennings"
+      }
+    ],
+    "person3": [
+      {
+        "first": "Rori",
+        "last": "Rohlfs"
+      }
+    ]
+  }]            
+  module.exports = mydata
